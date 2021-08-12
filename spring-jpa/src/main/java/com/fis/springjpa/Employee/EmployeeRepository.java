e586c8f2ff85c1e24755e191f9e2ad4db55df20d
Meghana66 <meghana.shetty.105@gmail.com>
2021-08-12 12:28:33 +0530

    added CRUD User by testing in postman

--- spring-jpa/src/main/java/com/fis/springjpa/Employee/EmployeeRepository.java
+++ spring-jpa/src/main/java/com/fis/springjpa/Employee/EmployeeRepository.java
@@ -3,7 +3,6 @@ package com.fis.springjpa.Employee;
 import org.springframework.data.repository.CrudRepository;
 
 
-
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
 }
