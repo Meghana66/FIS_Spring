188fe2c050b6ea52cd121c8fd7300c89db8d0f99
Meghana66 <meghana.shetty.105@gmail.com>
2021-08-13 13:57:44 +0530

    added calculator using lambda

---
+++ testing-web/src/main/java/com/fis/testingweb/HomeController.java
@@ -0,0 +1,17 @@
+package com.fis.testingweb;
+
+//package com.example.testingweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, Meghana";
+	}
+
+}
